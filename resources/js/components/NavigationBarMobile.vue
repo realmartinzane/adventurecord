76557d19b75e9ec683654557ec9aeb760fadde0b
Martin Zane <realmartinzane@gmail.com>
2019-07-24 09:06:29 +0200

    Make Navigation Bar Responsive

--- resources/js/components/NavigationBarMobile.vue
+++ resources/js/components/NavigationBarMobile.vue
@@ -12,6 +12,15 @@
                         <span>Log in</span>
                     </b-button>
                 </b-nav-item>
+                <b-nav-item-dropdown toggle-class="adv-toggle" right>
+                    <template slot="button-content"><font-awesome-icon :icon="['fas', 'bars']"></font-awesome-icon></template>
+                    <b-dropdown-item>Home</b-dropdown-item>
+                    <b-dropdown-item>Updates</b-dropdown-item>
+                    <b-dropdown-item>Commands</b-dropdown-item>
+                    <b-dropdown-item>Tutorial</b-dropdown-item>
+                    <b-dropdown-item>Wiki</b-dropdown-item>
+                    <b-dropdown-item>Marketplace</b-dropdown-item>
+                </b-nav-item-dropdown>
             </b-navbar-nav>
         </b-navbar>
     </b-row>
@@ -23,42 +32,49 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 @import '../../sass/_mixins.scss';
 
-    .navbar {background-color: #10121A; width: 100%;}
-    .navbar-brand {img{height: 50px;}}
-    .navbar-nav .nav-item
+    .navbar-mobile
     {
-        margin: 0 10px;
-        a
+        position: relative;
+        z-index: 3;
+        .navbar
         {
-            font-family: 'Roboto Condensed', sans-serif;
-            font-weight: bold;
-            color: white;
-            &:hover, &:active, &:focus {color: #DCC458;}
-        }
-        button
+            position: relative;
+            z-index: 2;
+            background-color: #10121A; 
+            width: 100%;
+            }
+        .navbar-brand {img{height: 50px;}}
+        .navbar-nav .nav-item
         {
-            text-transform: uppercase;
-            background-color: #7289da;
-            svg {vertical-align: middle}
-        }
-        .adv-link 
-        {
-            border: none; 
-            background: transparent;
-            font-size: 1.3rem;
-            @include transition(all 100ms);
-            @include transform(scale(1.4));
-            vertical-align: middle;
-            &:hover
+            margin: 0 10px;
+            a
             {
-                @include transform(scale(1.6));
+                font-family: 'Roboto Condensed', sans-serif;
+                font-weight: bold;
+                color: white;
+                &:hover, &:active, &:focus {color: #DCC458;}
+            }
+            button
+            {
+                text-transform: uppercase;
+                background-color: #7289da;
+                svg {vertical-align: middle}
             }
         }
-
+        .dropdown-menu 
+        {
+            background: #212127;
+            .dropdown-item
+            {
+                padding-top: 7.5px;
+                padding-bottom: 7.5px;
+                &:hover {background: #2d2d35}
+            }
+        }
     }
 
 </style>
