5e80b67ee3537bc50aa3ec5938c5b203cbb3472a
Martin Zane <realmartinzane@gmail.com>
2019-08-12 12:32:21 +0200

    Remove old code from User Settings

--- resources/js/components/UserSettings.vue
+++ resources/js/components/UserSettings.vue
@@ -16,28 +16,6 @@
             </div>
         </form>
     </section>
-    <!--
-    <b-row class="settings">
-        <section-header-component 
-            :text="'VampY - Settings'"
-            :spacing="true">
-        </section-header-component>
-        <b-col cols="10" offset="1" lg="8" offset-lg="2" xl="6" offset-xl="3">
-            <b-form class="adv-form">
-                <b-form-group id="language-group" label="Language:" label-for="language">
-                    <b-form-select
-                        id="language"
-                        :options="languages"
-                        value="en"
-                        :select-size="3"
-                    ></b-form-select>
-                </b-form-group>
-
-                <b-button type="submit" class="adv-btn">Submit</b-button>
-            </b-form>
-        </b-col>
-    </b-row>
-    -->
 </template>
 
 <script>
